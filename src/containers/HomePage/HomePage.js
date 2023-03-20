e515fc09aecf165243085ac97d87cf66325c805a
hieulb@fabbi.com.vn <hieulb@fabbi.com.vn>
2023-03-20 15:26:02 +0700

    video 65

---
+++ src/containers/HomePage/HomePage.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import HomeHeader from "./HomeHeader";
+import Specialty from "./Section/Specialty";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./HomePage.scss";
+import MedicalFacility from "./Section/MedicalFacility";
+import OutStandingDoctor from "./Section/OutStandingDoctor";
+import HandBook from "./Section/HandBook";
+
+class HomePage extends Component {
+  render() {
+    let settings = {
+      dots: false,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+    };
+    return (
+      <div>
+        <HomeHeader />
+        <Specialty settings={settings} />
+        <MedicalFacility settings={settings} />
+        <OutStandingDoctor settings={settings} />
+        <HandBook settings={settings} />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
